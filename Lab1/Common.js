c97c49cc24c22d294259afa3998c22b60932a5b8
siddharthvrma <sidverma1199@gmail.com>
2020-03-14 16:06:39 +0530

    first commit

---
+++ Lab1/Common.js
@@ -0,0 +1,9 @@
+var msg;
+msg="<p><code>The actual script is in external script file called common.js</code></p>";
+
+function addNos(headVar,bodyVar)
+{
+    document.write(this.msg)
+    document.write("The sum of the variables <i>headVar</i> and <i>bodyVar</i> is "+(headVar+bodyVar))
+
+}
